7a660cdee25ba9dea1582c7b5f830ed310a5e215
Musa Kurt <hackedtim61@gmail.com>
2019-09-11 15:16:21 +0300

    Optimized modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import _Valinode from "./lib";
-class __Valinode extends _Valinode {}
-global.Valinode = __Valinode
-export default __Valinode;
+import Valinode from "./lib";
+export { Valinode }
+export default Valinode
 // TODO: date formatında required kuralında ayın 31. gününde boş hatası vermesi düzeltilmelidir. 
 // TODO: date formatı için "bu günden büyük, bu günden küçük" kuralı eklenmelidir.
