0e1535f9ea8234bbe99bbe3a85a96f5fabfe5f97
Musa Kurt <hackedtim61@gmail.com>
2019-09-11 15:16:01 +0300

    parallel-webpack activated

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,38 @@
 const path = require('path');
+const { createVariants } = require('parallel-webpack');
 
-module.exports = {
+const variants = {
+    minify: [true, false]
+}
+
+const baseConfig = {
     entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'valinode.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-      }
-    ]
-    }
+    mode: process.env.NODE_ENV,
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
 };
+
+const configFactory = (options) => {
+    return {
+        entry: options.entry,
+        module: options.module,
+        mode: options.mode,
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: `valinode${options.minify ? '.min' : ''}.js`,
+            library: 'Valinode',
+            libraryTarget: 'umd'
+        }
+    }
+}
+
+module.exports = createVariants(baseConfig, variants, configFactory)
